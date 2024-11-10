517bd8ebb44ec205c776758bfc127f9bf283ddc8
Joel Butler <joel.e.butler04@gmail.com>
2024-11-09 22:46:17 -0500

    Soundtrack implementation
    Play during gameplay

--- Penguini/Assets/Scripts/MusicTimer.cs
+++ Penguini/Assets/Scripts/MusicTimer.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class MusicTimer : MonoBehaviour
 {
     public float timeBetweenPlays = 30f; //30 seconds for you
+    private int trackNum = 0;
     private float time;
+    private AudioSource[] audioSources;
 
     public void Start() {
         time = timeBetweenPlays;
+        audioSources = GetComponents<AudioSource>();
     }
     public void Update()
     {
@@ -14,8 +17,14 @@ public class MusicTimer : MonoBehaviour
             time -= Time.deltaTime;
         }
         else {
-            GetComponent<AudioSource>().Play();
+            audioSources[trackNum].Play();
             time = timeBetweenPlays;
+            if (trackNum < audioSources.Length) {
+                trackNum += 1;
+            }
+            else {
+                trackNum = 0;
+            }
         }
     }
 }
